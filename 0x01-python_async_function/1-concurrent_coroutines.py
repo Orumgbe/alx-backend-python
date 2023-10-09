d90b9e0e87097ccc1b7e406c4f02c00139523b36
Orumgbe <orumgbeugbekile@gmail.com>
2023-10-09 21:51:28 +0100

    task runtime list

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Returns list in order of time taken for task completion"""
     wait_time = []
