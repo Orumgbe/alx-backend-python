ea21a5ac7e985fca43b4f03050bdf3eabef98b8a
Orumgbe <orumgbeugbekile@gmail.com>
2023-10-10 01:45:18 +0100

    floor float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module contains floor function"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns value of rounded down float"""
+    return math.floor(n)
