3057ce6a612af1cf988b929940f79ed6df874314
Orumgbe <orumgbeugbekile@gmail.com>
2023-10-10 01:10:38 +0100

    3 and 4 asyncio tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" asynchronous coroutine waits for a random delay """
+from typing import List
+import asyncio
+import random
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns list in order of time taken for task completion"""
+    wait_time = []
+    asc_order = []
+    for calls in range(n):
+        wait_time.append(task_wait_random(max_delay))
+    for fastest in asyncio.as_completed(wait_time):
+        asc_order.append(await fastest)
+    return asc_order
